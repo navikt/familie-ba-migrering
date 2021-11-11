9c2a7d970416237e6efb793dcfd48d4c4ae6fabe
Meng Zhu <meng.zhu@nav.no>
2021-11-11 13:23:05 +0100

    test trigger

--- src/main/kotlin/no/nav/familie/ba/migrering/rest/TestController.kt
+++ src/main/kotlin/no/nav/familie/ba/migrering/rest/TestController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/test")
 @Unprotected
-@Profile("preprod-fake")
+@Profile("preprod")
 class TestController(val bekreftMigreringService: BekreftMigreringService) {
 
     @GetMapping(path = ["/triggerBekreft"], produces = [MediaType.APPLICATION_JSON_VALUE])
