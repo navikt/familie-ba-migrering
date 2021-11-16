be0146fc2d8fe1c280775ac783951ab16929edaa
Henning Solberg <henning.solberg@nav.no>
2021-11-16 09:25:37 +0100

    Setter migreringstidspunkt tilbake til kl 8

--- src/main/kotlin/no/nav/familie/ba/migrering/services/HentSakTilMigreringService.kt
+++ src/main/kotlin/no/nav/familie/ba/migrering/services/HentSakTilMigreringService.kt
@@ -20,7 +20,7 @@ class HentSakTilMigreringService(
     @Value("\${migrering.aktivert:false}") val migreringAktivert: Boolean
 ) {
 
-    @Scheduled(cron = "0 * 8 * * ?", zone = "Europe/Oslo")
+    @Scheduled(cron = "0 0 8 * * ?", zone = "Europe/Oslo")
     fun hentSakTilMigrering() {
         if (!migreringAktivert) {
             Log.info("Migrering deaktivert, stopper videre jobbing")
