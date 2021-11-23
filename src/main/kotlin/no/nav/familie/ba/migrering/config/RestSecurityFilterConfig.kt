c9f2298438d4bac29ce107131950e1401cb3bbba
Tor Nærland <tor.nerland@nav.no>
2021-11-23 10:26:42 +0100

    Flytter no.nav.security.jwt config opp til application.yaml nivå. Fjerner midlertidig inbound access policy for ba-sak.

--- src/main/kotlin/no/nav/familie/ba/migrering/config/RestSecurityFilterConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/migrering/config/RestSecurityFilterConfig.kt
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Configuration
-class SecurityConfig() {
+class RestSecurityFilterConfig {
 
     @Bean
     fun requestFilter() = object : OncePerRequestFilter() {
