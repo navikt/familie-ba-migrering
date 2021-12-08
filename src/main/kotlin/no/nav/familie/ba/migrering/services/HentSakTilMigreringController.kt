275aecd2738bb3ed4d9689e69abb8298d69e4b99
Stig Helge Strøm <stig.helge.strom@nav.no>
2021-12-08 12:26:18 +0100

    Controller for å trigge migrering manuelt

---
+++ src/main/kotlin/no/nav/familie/ba/migrering/services/HentSakTilMigreringController.kt
@@ -0,0 +1,30 @@
+package no.nav.familie.ba.migrering.services
+
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+
+@RestController
+@RequestMapping("/api/migrer")
+@ProtectedWithClaims(issuer = "azuread")
+@Validated
+class HentSakTilMigreringController(
+    private val hentSakTilMigreringService: HentSakTilMigreringService,
+) {
+
+
+    @PostMapping("/start")
+    fun migrer(@Valid @RequestBody startMigreringRequest: StartMigreringRequest) {
+        hentSakTilMigreringService.migrer(startMigreringRequest.antallPersoner)
+    }
+
+    data class StartMigreringRequest(@Min(1) @Max(20) val antallPersoner: Int)
+
+
+}
