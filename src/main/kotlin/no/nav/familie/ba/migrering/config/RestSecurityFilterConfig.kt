6c199892ffa319b9a1c7ac69525570ad9e363aaa
Stig Strøm <stig.helge.strom@nav.no>
2021-12-09 10:24:25 +0100

    Mer logging av filter (#57)
    * Mer logging av filter
    
    * Bruker annen forvaltningsrolle i preprod

--- src/main/kotlin/no/nav/familie/ba/migrering/config/RestSecurityFilterConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/migrering/config/RestSecurityFilterConfig.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ba.migrering.config
 
 import no.nav.familie.sikkerhet.ClientTokenValidationFilter
 import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -14,6 +15,8 @@ import javax.servlet.http.HttpServletResponse
 class RestSecurityFilterConfig(@Value("\${rolle.teamfamilie.forvalter}")
                                val forvalterRolleTeamfamilie: String) {
 
+    private val secureLogger = LoggerFactory.getLogger("secureLogger")
+
     @Bean
     fun requestFilter() = object : OncePerRequestFilter() {
         override fun doFilterInternal(
@@ -21,18 +24,29 @@ class RestSecurityFilterConfig(@Value("\${rolle.teamfamilie.forvalter}")
             response: HttpServletResponse,
             filterChain: FilterChain
         ) {
-            val clientTokenValidationFilter = ClientTokenValidationFilter(true, true)
-            clientTokenValidationFilter.doFilter(request, response, filterChain)
-            if (!hentGrupper().contains(forvalterRolleTeamfamilie) && !environment.activeProfiles.contains("dev")) {
+            val grupper = hentGrupper()
+            if (!grupper.contains(forvalterRolleTeamfamilie) && !environment.activeProfiles.contains("dev")) {
+                secureLogger.info("Ugyldig rolle for url=${request.requestURI} grupper=$grupper, forvalterRolleTeamfamilie=$forvalterRolleTeamfamilie")
                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Handling krever teamfamilie forvalter rolle")
+            } else {
+                try {
+                    val clientTokenValidationFilter = ClientTokenValidationFilter(true, true)
+                    clientTokenValidationFilter.doFilter(request, response, filterChain)
+                } catch (e: Exception) {
+                    secureLogger.warn("Uventet feil i doFilter for url=${request.requestURI} grupper=$grupper", e)
+                    throw e
+                }
             }
         }
 
-        override fun shouldNotFilter(request: HttpServletRequest) =
-            request.requestURI.contains("/internal") ||
+        override fun shouldNotFilter(request: HttpServletRequest): Boolean {
+            val shouldNotFilter = request.requestURI.contains("/internal") ||
                     request.requestURI.startsWith("/api/task") ||
                     request.requestURI.startsWith("/swagger") ||
-                    request.requestURI.startsWith("/v3") // i bruk av swagger
+                    request.requestURI.startsWith("/v3")
+            logger.info("Should not filter returns $shouldNotFilter for ${request.requestURI}")
+            return shouldNotFilter
+        } // i bruk av swagger
     }
 
     private fun hentGrupper(): List<String> {
