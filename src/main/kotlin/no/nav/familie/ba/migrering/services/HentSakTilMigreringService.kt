e4be41250b4a0c0ad4950cbbdf53e1e92d3fce63
Stig Helge Strøm <stig.helge.strom@nav.no>
2021-12-09 15:00:23 +0100

    Hent flere migreringer hvis maks antall ikke er nådd

--- src/main/kotlin/no/nav/familie/ba/migrering/services/HentSakTilMigreringService.kt
+++ src/main/kotlin/no/nav/familie/ba/migrering/services/HentSakTilMigreringService.kt
@@ -31,24 +31,35 @@ class HentSakTilMigreringService(
             Log.info("Migrering deaktivert, stopper videre jobbing")
             return "Migrering deaktivert, stopper videre jobbing"
         }
-        val personerForMigrering = infotrygdClient.hentPersonerKlareForMigrering(
-            MigreringRequest(
-                page = 4,
-                size = ANTALL_PERSONER_SOM_HENTES_FRA_INFOTRYGD,
-                valg = "OR",
-                undervalg = "OS",
-                maksAntallBarn = 1,
-            )
-        )
-
-        if (personerForMigrering.size > ANTALL_PERSONER_SOM_HENTES_FRA_INFOTRYGD) {
-            Log.error("For manger personer (${personerForMigrering.size}) avbryter migrering")
-            return "For manger personer (${personerForMigrering.size}) avbryter migrering"
-        }
-
-        Log.info("Fant ${personerForMigrering.size} personer for migrering")
 
         var antallPersonerMigrert = 0
+        var startSide = 4
+        while (antallPersonerMigrert < antallPersoner) {
+            val personerForMigrering = infotrygdClient.hentPersonerKlareForMigrering(
+                MigreringRequest(
+                    page = startSide,
+                    size = ANTALL_PERSONER_SOM_HENTES_FRA_INFOTRYGD,
+                    valg = "OR",
+                    undervalg = "OS",
+                    maksAntallBarn = 1,
+                )
+            )
+            Log.info("Fant ${personerForMigrering.size} personer for migrering på side $startSide")
+            if (personerForMigrering.isEmpty()) break
+
+            antallPersonerMigrert = oppretteEllerSkipMigrering(personerForMigrering, antallPersonerMigrert)
+            if (antallPersonerMigrert < antallPersoner) {
+                startSide++
+            }
+        }
+
+
+
+        return "Migrerte $antallPersoner"
+    }
+
+    private fun oppretteEllerSkipMigrering(personerForMigrering: Set<String>, antallAlleredeMigret: Int): Int {
+        var antallPersonerMigrert = antallAlleredeMigret
         for (person in personerForMigrering) {
             if (!existByPersonIdentAndStatusIn(
                     person,
@@ -59,13 +70,15 @@ class HentSakTilMigreringService(
                     )
                 )
             ) {
-                taskRepository.save(MigreringTask.opprettTask(MigreringTaskDto(person)))
+//                taskRepository.save(MigreringTask.opprettTask(MigreringTaskDto(person)))
+                secureLogger.info("Oppretter migrering for person $person")
                 antallPersonerMigrert++
-            }
+            } else secureLogger.info("Personen $person er allerede forsøkt migrert")
+
             if (antallPersonerMigrert == antallPersoner)
-                break
+                return antallPersonerMigrert
         }
-        return "Migrerte $antallPersoner"
+        return antallPersonerMigrert
     }
 
     private fun existByPersonIdentAndStatusIn(ident: String, status: List<MigreringStatus>): Boolean {
@@ -74,6 +87,7 @@ class HentSakTilMigreringService(
 
     companion object {
         val Log = LoggerFactory.getLogger(HentSakTilMigreringService::class.java)
+        private val secureLogger = LoggerFactory.getLogger("secureLogger")
         val ANTALL_PERSONER_SOM_HENTES_FRA_INFOTRYGD = 200
     }
 }
