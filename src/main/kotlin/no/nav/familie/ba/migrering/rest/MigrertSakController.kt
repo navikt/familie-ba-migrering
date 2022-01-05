9ccb8e3f599c9a9097ac368228298b38fb914782
Stig Helge Strøm <stig.helge.strom@nav.no>
2022-01-05 15:25:46 +0100

    Endepunkt for vedlikehold

--- src/main/kotlin/no/nav/familie/ba/migrering/rest/MigrertSakController.kt
+++ src/main/kotlin/no/nav/familie/ba/migrering/rest/MigrertSakController.kt
@@ -1,7 +1,11 @@
+@file:Suppress("unused")
+
 package no.nav.familie.ba.migrering.rest
 
+import no.nav.familie.ba.migrering.domain.MigreringStatus
 import no.nav.familie.ba.migrering.domain.Migrertsak
 import no.nav.familie.ba.migrering.domain.MigrertsakRepository
+import no.nav.familie.ba.migrering.domain.TellFeilResponse
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.validation.annotation.Validated
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 import javax.validation.Valid
@@ -25,8 +30,19 @@ class MigrertSakController(
 
     @GetMapping("/")
     @Transactional(readOnly = true)
-    fun hentAlleSaker(): List<Migrertsak> {
-        return migrertsakRepository.findAll().toList()
+    fun hentAlleSaker(@RequestParam(required = false) status: List<MigreringStatus>): MigrertSakResponse {
+        return if (status.isEmpty()) {
+            MigrertSakResponse(migrertsakRepository.findAll().toList())
+        } else {
+            MigrertSakResponse(migrertsakRepository.findByStatusIn(status))
+        }
+    }
+
+    @GetMapping("/tell-feilet")
+    @Transactional(readOnly = true)
+    fun tellAntallFeiledeMigrering(): List<TellFeilResponse> {
+        return migrertsakRepository.tellFeiledeMigrerteSaker()
+
     }
 
     @GetMapping("/{id}")
@@ -47,4 +63,10 @@ class MigrertSakController(
         migrertsakRepository.deleteById(id)
     }
 
+
+}
+
+class MigrertSakResponse(migrerteSaker: List<Migrertsak>) {
+    val total: Int = migrerteSaker.size
+    val data: List<Migrertsak> = migrerteSaker
 }
