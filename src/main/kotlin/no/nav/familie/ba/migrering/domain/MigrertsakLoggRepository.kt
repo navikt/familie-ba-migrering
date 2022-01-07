697ee93cf220eba91ef606fe6f3ae551d644f379
Stig Strøm <stig.helge.strom@nav.no>
2022-01-07 16:09:26 +0100

    Rekjør feilede basert på feiltype eller ident (#82)
    * Rekjør feilede basert på feiltype eller ident

---
+++ src/main/kotlin/no/nav/familie/ba/migrering/domain/MigrertsakLoggRepository.kt
@@ -0,0 +1,11 @@
+package no.nav.familie.ba.migrering.domain
+
+import no.nav.familie.ba.migrering.domain.common.InsertUpdateRepository
+import no.nav.familie.ba.migrering.domain.common.RepositoryInterface
+import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
+
+@Repository
+@Transactional
+interface MigrertsakLoggRepository : RepositoryInterface<MigrertsakLogg, UUID>, InsertUpdateRepository<MigrertsakLogg>
