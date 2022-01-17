80aad5a42e2ff71cdf881c99b1052b15de2c409c
Stig Helge Strøm <stig.helge.strom@nav.no>
2022-01-17 15:22:04 +0100

    Endepunkt for å søke på person og utvidelser av findAll til maks 500 med paging

--- src/main/kotlin/no/nav/familie/ba/migrering/domain/common/RepositoryInterface.kt
+++ src/main/kotlin/no/nav/familie/ba/migrering/domain/common/RepositoryInterface.kt
@@ -1,11 +1,12 @@
 package no.nav.familie.ba.migrering.domain.common
 
-import org.springframework.data.repository.CrudRepository
+
 import org.springframework.data.repository.NoRepositoryBean
+import org.springframework.data.repository.PagingAndSortingRepository
 
 /**
  * På grunn av att vi setter id's på våre entitetet så prøver spring å oppdatere våre entiteter i stedet for å ta insert
  */
 @NoRepositoryBean
-interface RepositoryInterface<T, ID> : CrudRepository<T, ID>
+interface RepositoryInterface<T, ID> : PagingAndSortingRepository<T, ID>
 
