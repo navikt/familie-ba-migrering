4d2e3068818578e7a428473e2fa9e5dae70cf5a6
Henning Solberg <henning.solberg@nav.no>
2022-02-03 14:42:04 +0100

    øker til 5 barn og 1000 personer og starter på side 1

--- src/main/kotlin/no/nav/familie/ba/migrering/services/HentSakTilMigreringService.kt
+++ src/main/kotlin/no/nav/familie/ba/migrering/services/HentSakTilMigreringService.kt
@@ -28,7 +28,7 @@ class HentSakTilMigreringService(
         }
 
         var antallPersonerMigrert = 0
-        var startSide = 4
+        var startSide = 1
         while (antallPersonerMigrert < antallPersoner) {
             val personerForMigrering = infotrygdClient.hentPersonerKlareForMigrering(
                 MigreringRequest(
@@ -118,8 +118,8 @@ class HentSakTilMigreringService(
 
         val Log = LoggerFactory.getLogger(HentSakTilMigreringService::class.java)
         private val secureLogger = LoggerFactory.getLogger("secureLogger")
-        const val ANTALL_PERSONER_SOM_HENTES_FRA_INFOTRYGD = 500
-        const val MAX_ANTALL_BARN = 2
+        const val ANTALL_PERSONER_SOM_HENTES_FRA_INFOTRYGD = 1000
+        const val MAX_ANTALL_BARN = 5
         const val MIGRERING_DEAKTIVERT_MELDING = "Migrering deaktivert, stopper videre jobbing"
 
     }
