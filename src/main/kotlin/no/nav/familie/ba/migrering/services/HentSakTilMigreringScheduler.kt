df8364148a458c82d386fd830c32b04485e0703a
Stig Strøm <stig.helge.strom@nav.no>
2022-03-28 10:39:40 +0200

    Service for å trigge migrering av delt bosted saker (#149)

--- src/main/kotlin/no/nav/familie/ba/migrering/services/HentSakTilMigreringScheduler.kt
+++ src/main/kotlin/no/nav/familie/ba/migrering/services/HentSakTilMigreringScheduler.kt
@@ -1,6 +1,8 @@
 package no.nav.familie.ba.migrering.services
 
+import no.nav.familie.ba.migrering.services.Kategori.ORDINÆR_DELT_BOSTED
 import no.nav.familie.ba.migrering.services.Kategori.UTVIDET
+import no.nav.familie.ba.migrering.services.Kategori.UTVIDET_DELT_BOSTED
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
@@ -9,20 +11,34 @@ import org.springframework.stereotype.Service
 @Service
 class HentSakTilMigreringScheduler(
     val hentSakTilMigreringService: HentSakTilMigreringService,
-    @Value("\${migrering.antallPersoner.ordinær}") val antallPersoner: Int,
-    @Value("\${migrering.antallPersoner.utvidet}") val antallPersonerUtvidetBa: Int,
+    @Value("\${migrering.antallPersoner.ordinær}") val antallPersoneOrdinærr: Int,
+    @Value("\${migrering.antallPersoner.utvidet}") val antallPersonerUtvidet: Int,
+    @Value("\${migrering.antallPersoner.ordinær.deltbosted:0}") val antallPersonerOrdinærDeltBosted: Int,
+    @Value("\${migrering.antallPersoner.utvidet.deltbosted:0}") val antallPersonerUtvidetDeltBosted: Int,
 ) {
 
     @Scheduled(cron = "0 0 13 * * MON-FRI", zone = "Europe/Oslo")
     fun hentOrdinærSakTilMigreringScheduler() {
-        Log.info("Trigger migrering av $antallPersoner")
-        hentSakTilMigreringService.migrer(antallPersoner)
+        Log.info("Trigger migrering av $antallPersoneOrdinærr ordinære saker")
+        hentSakTilMigreringService.migrer(antallPersoneOrdinærr)
     }
 
     @Scheduled(cron = "0 50 12 * * MON-FRI", zone = "Europe/Oslo")
     fun hentUtvidetSakerTilMigreringScheduler() {
-        Log.info("Trigger migrering av $antallPersoner")
-        hentSakTilMigreringService.migrer(antallPersonerUtvidetBa, kategori = UTVIDET)
+        Log.info("Trigger migrering av $antallPersonerUtvidet utvidete saker")
+        hentSakTilMigreringService.migrer(antallPersonerUtvidet, kategori = UTVIDET)
+    }
+
+    @Scheduled(cron = "0 40 12 * * MON-FRI", zone = "Europe/Oslo")
+    fun hentDeltBostedScheduler() {
+        Log.info("Trigger migrering av $antallPersonerOrdinærDeltBosted ordinære saker med delt bosted")
+        if (antallPersonerOrdinærDeltBosted > 0) {
+            hentSakTilMigreringService.migrer(antallPersonerOrdinærDeltBosted, kategori = ORDINÆR_DELT_BOSTED)
+        }
+        Log.info("Trigger migrering av $antallPersonerUtvidetDeltBosted utvidete saker med delt bosted")
+        if (antallPersonerUtvidetDeltBosted > 0) {
+            hentSakTilMigreringService.migrer(antallPersonerUtvidetDeltBosted, kategori = UTVIDET_DELT_BOSTED)
+        }
     }
 
 
